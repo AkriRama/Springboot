e4c569d6fc98fa784567ddec601a6c3ffa73b2a7
AkriR <114977574+AkriRama@users.noreply.github.com>
2024-12-23 22:00:58 +0700

    Update UserLoginRepository.java
    Add DeleteById

--- transactions/src/main/java/com/example/transcations/transactions/repository/UserLoginRepository.java
+++ transactions/src/main/java/com/example/transcations/transactions/repository/UserLoginRepository.java
@@ -1,9 +1,11 @@
 package com.example.transcations.transactions.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.transcations.transactions.model.UserLogin;
 
@@ -11,5 +13,10 @@ import com.example.transcations.transactions.model.UserLogin;
 public interface UserLoginRepository extends JpaRepository<UserLogin, Integer> {
     @Query("SELECT ul FROM UserLogin ul WHERE ul.id = :id")
     public UserLogin get(@Param(value = "id") Integer id);
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM UserLogin ul WHERE ul.id = ?1")
+    public Integer delete(Integer userLoginId);
 
 }
