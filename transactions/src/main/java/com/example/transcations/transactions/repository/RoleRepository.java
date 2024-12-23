91c4a4b2bdc517f737d0c9b5d0272987ce105d52
AkriR <114977574+AkriRama@users.noreply.github.com>
2024-12-23 22:01:30 +0700

    Update RoleRepository.java
    Add getLevelById

--- transactions/src/main/java/com/example/transcations/transactions/repository/RoleRepository.java
+++ transactions/src/main/java/com/example/transcations/transactions/repository/RoleRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.transcations.transactions.model.Role;
@@ -14,8 +15,15 @@ public interface RoleRepository extends JpaRepository<Role, Integer> {
     // public long count();
 
     // @Query(value = "SELECT id, name FROM tb_role", nativeQuery = true)
-    // public List<Role> get();
+    @Query(value = "SELECT id, name, level FROM tb_roles", nativeQuery = true)
+    public List<Role> get();
 
     @Query(value = "SELECT * FROM tb_roles  WHERE level = 1", nativeQuery = true)
     public Role getLevel1();
+
+    @Query(value = "SELECT * FROM tb_roles  WHERE id = ?", nativeQuery = true)
+    public Role getLevelById(@Param(value = "id") Integer id);
+
+    // @Query("SELECT r.id, r.name, r.level, ul FROM UserLogin ul JOIN ul.role r")
+    // public List<Role> get();
 }
